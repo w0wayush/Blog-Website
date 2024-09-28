e7c4ebb29b0b1a986a882a189856f104f379719c
w0wayush <ayupandey82@gmail.com>
2024-09-28 18:14:13 +0530

    Added blogSkeleton when  fetching data from backend

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,7 +1,32 @@
- type Props = {};
+import { Appbar } from "../components/Appbar";
+import { FullBlog } from "../components/FullBlog";
+import { Spinner } from "../components/Spinner";
+import { useBlog } from "../hooks";
+import { useParams } from "react-router-dom";
 
-const Blog = (props: Props) => {
-  return <div>Blog</div>;
+// atomFamilies/selectorFamilies
+export const Blog = () => {
+  const { id } = useParams();
+  const { loading, blog } = useBlog({
+    id: id || "",
+  });
+
+  if (loading || !blog) {
+    return (
+      <div>
+        <Appbar />
+
+        <div className="h-screen flex flex-col justify-center">
+          <div className="flex justify-center">
+            <Spinner />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <FullBlog blog={blog} />
+    </div>
+  );
 };
-
-export default Blog;
